eee8cef758b30095ac73ba8fe9003ee9e0bb2a7a
Eslam_Nawara <67752395+Eslam-Nawara@users.noreply.github.com>
2022-06-19 23:11:48 +0200

    Update and rename helpers.h to src/helpers.h

--- src/helpers.h
+++ src/helpers.h
@@ -1,15 +1,11 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
-/* Here are helper routines that we've provided for you */
-
 void usage(void);
 
 void unix_error(char *msg);
 
 void app_error(char *msg);
-
-typedef void handler_t(int);
 
 int parseline(const char *cmdline, char **argv);
 
